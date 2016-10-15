0f3dd7ea99cc9937938e86817aa49b7386c17106
CoffeeKat <kat.delgado@outlook.com>
2016-10-15 13:45:02 -0500

    started using sessions for sign in variables

--- coffeekatastrophe/app.py
+++ coffeekatastrophe/app.py
@@ -1,15 +1,21 @@
 #Module includes
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session
+from flask_hashing import Hashing
 from flaskext.mysql import MySQL
 import os
 
 #Initializations
 app = Flask(__name__)
+app.secret_key = os.environ['SESSION_SECRET_KEY']
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = os.environ['MYSQL_PASS']
-app.config['MYSQL_DATABASE_DB'] = 'BlogPosts'
+app.config['MYSQL_DATABASE_DB'] = 'ProjBlog'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['HASHING_METHOD'] = 'sha256'
+mysql = MySQL()
 mysql.init_app(app)
+hashing = Hashing(app)
+hashing.init_app(app)
 
 
 #----------------------------------
@@ -35,13 +41,7 @@ def getBlogs(proj, page_number):
 ################################################################
 @app.route('/', methods=['GET', 'POST'])
 def main():
-    admin_val = None
-    cursor = conn.cursor()
-    if request.method == 'POST':
-        admin_val = "admin"
-    else:
-        admin_val = None
-    return render_template('Blog.html', admin = admin_val)
+    return render_template('Blog.html', admin = session.get('admin'))
 
 ################################################################
 # proj_blog
@@ -90,17 +90,48 @@ def addPost():
 #----------------------------------
 #User manipulation routes go here
 #----------------------------------
-@app.route('/login')
+@app.route('/signin')
 def signIn():
-    return "Page to log into blog"
+    return render_template('SignIn.html', admin = session.get('admin'))
 
 @app.route('/signup')
 def signUp():
-    return "Page to add user to blog"
+    return render_template('SignUp.html', admin = session.get('admin'))
 
 @app.route('/validateuser', methods = ['POST'])
 def validateUser():
-    return "Redirects to main blog in user session"
+    global admin_val
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.callproc('sp_validateLogin', (request.form['inputUsername'],))
+    data = cursor.fetchall()
+
+    if hashing.check_value(str(data[0][4]), request.form['inputPassword'], salt=os.environ['HASH_SALT']):
+
+        if request.form['inputUsername'] == os.environ['BLOG_ADMIN_USERNAME']:
+            session['admin'] = "Admin"
+        else:
+            session['admin'] = None
+
+    return redirect("/")
+
+@app.route('/createuser', methods = ['POST'])
+def createUser():
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    if request.method == 'POST':
+
+        val_name = request.form['inputName']
+        val_username = request.form['inputUsername']
+        val_email = request.form['inputEmail']
+        val_hash = hashing.hash_value(request.form['inputPassword'], salt=os.environ['HASH_SALT'])
+
+        cursor.callproc('sp_createUser',(val_name, val_username, val_email, val_hash))
+        data = cursor.fetchall()
+        print(data)
+        if len(data) is 0:
+            conn.commit()
+    return redirect("/")
 
 
 #Start server if called by itself
